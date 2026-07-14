7dc4482141371cf68366fe8d341a725f78a594fa
Fatine-I <f.icyimpaye@alustudent.com>
2026-07-14 11:54:37 +0400

    committng Quest2 file

---
+++ Quest2.py
@@ -0,0 +1,10 @@
+#Check result(mark)
+def check_result(mark):
+    if mark>=50:
+        print("Pass")
+    else:
+        print("Fail")     
+m=input("What are your marks? ")
+if not 0<=m<=100:
+    raise ValueError(f"invalid mark!\n marks should range between 0 and 100")
+print(check_result(m))
