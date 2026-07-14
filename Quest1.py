7e7d8dc414993156bff16ec9ffe2a7f4d4a6e08a
Fatine-I <f.icyimpaye@alustudent.com>
2026-07-14 11:05:05 +0400

    Committing Quest1 file

---
+++ Quest1.py
@@ -0,0 +1,14 @@
+#The Gate of Numbers
+def check_even_odd(number):
+    if number%2==0:
+        print (f"{number} is even.")
+    else:
+        print (f"{number} is odd.")
+num=input("Provide any integer of your choice ")
+while isinstance(num, int) is false:
+    if isinstance(num, int) is false:
+        print("The number should be an integer")
+        num=input("Input an integer number ")
+    else:
+            break
+print(check_even_odd(num))        
