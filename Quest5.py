a1a1765bda6a0c500e3ca8a35ea787e78a2696ba
Fatine-I <f.icyimpaye@alustudent.com>
2026-07-15 22:49:58 +0400

    committing Quest5 file

---
+++ Quest5.py
@@ -0,0 +1,25 @@
+#The Magic Calculator
+def add(n1,n2):
+    print(f"{n1} + {n2} = {n1+n2} ")
+def subtract(n1,n2):
+    print(f"{n1} - {n2} = {n1-n2} ")
+def multiply(n1,n2):
+    print(f"{n1} * {n2} = {n1*n2} ")
+def divide(n1,n2):
+    try:
+        print(f"{n1} / {n2} = {n1/n2} ")
+    except ZeroDivisionError:
+        print(f"Error occured: the second number is zero.")       
+num1=int(input("enter a number of your choice"))
+num2=int(input("enter another number"))
+sign=input("which operation: +,-,*,/")
+if sign=="+":
+    add(num1,num2)
+elif sign=="-":
+    subtract(num1,num2)
+elif sign=="*":
+    multiply(num1,num2)
+elif sign=="/":
+    divide(num1,num2)
+else:
+    raise TypeError("Error: unrecognized sign")    
